c9ad1472520943675157b8390ffdab709e87671b
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2022-01-09 15:45:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
 function setup() {
     var canvas = createCanvas(700, 600);
     canvas.parent('canvas');
@@ -20,5 +24,56 @@ function gotPoses(results) {
         rightWristX = results[0].pose.rightWrist.x;
         scoreRightWrist = results[0].pose.keypoints[10].score;
         console.log(scoreRightWrist);
+    }
+}
+
+game_status = "";
+
+function preload() {
+    ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+    missed = loadSound("missed.wav");
+}
+
+function startGame() {
+    GameStatus = "start";
+    document.getElementById("status").innerHTML = "Game is Loading!";
+}
+
+function draw() {
+    if (game_status == "start") {
+        background(0);
+        image(video, 0, 0, 700, 600);
+
+        fill("black");
+        stroke("black");
+        rect(680, 0, 20, 700);
+
+        fill("black");
+        stroke("black");
+        rect(0, 0, 20, 700);
+
+        if (scoreRightWrist > 0.2) {
+            fill("red");
+            stroke("red");
+            circle(rightWristX, rightWristY, 30);
+        }
+
+        paddleInCanvas();
+
+        fill(250, 0, 0);
+        stroke(0, 0, 250);
+        strokeWeight(0.5);
+        paddle1Y = rightWristY;
+        rect(paddle1X, paddle1Y, paddle1, paddle1height, 100);
+
+        fill("#FFA500");
+        stroke("#FFA500");
+        var paddle2y = ball.y - paddle2Height / 2;
+        rect(paddle2Y, paddle2y, paddle2, paddle2Height, 100);
+
+        midline();
+        drawScore();
+        models();
+        move();
     }
 }
